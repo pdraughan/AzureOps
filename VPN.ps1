adb7033db944a28f732512f71ff128a8b6d7b5df
Philip Draughan <Philip.Draughan@avenel.com>
2018-10-21 21:14:00 -0500

    Improved VPN scripts

--- VPN.ps1
+++ VPN.ps1
@@ -1,40 +1,33 @@
-﻿# use this to get all the VPN info
+﻿# use this collection of scripts to get all the VPN info
 $RG1          = "resource group name"
 $Connection16 = "Connection Name"
-$VNetName1 = "VNet Name"
-$GWName1 = "VPN Gateway name"
-
-
-$lngw = Get-AzureRmLocalNetworkGateway -Name "LocalNetworkGateway" -ResourceGroupName $RG1
 $connection6  = Get-AzureRmVirtualNetworkGatewayConnection -Name $Connection16 -ResourceGroupName $RG1
 
-$vnet1      = Get-AzureRmVirtualNetwork -Name $VNetName1 -ResourceGroupName $RG1
-$subnet1    = Get-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $vnet1
+# Uncomment the following group of variables if you are resizing
+<# $VNetName1 = "VNet Name"
+ $GWName1 = "VPN Gateway name"
+ $vngw1 = Get-AzureRmVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1
+#> 
+<# Resize the Gateway
+Resize-AzureVNetGateway -VNetName $vngw1.Name -GatewaySKU "HighPerformance"
+#>
 
-
-$vngw1 = Get-AzureRmVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1
-
-$newpolicy6 = New-AzureRmIpsecPolicy -IkeEncryption AES256 -IkeIntegrity SHA384 -DhGroup DHGroup24 `
-                                       -IpsecEncryption AES256 -IpsecIntegrity SHA256 -PfsGroup PFS24 `
+<# This section changes the VPN Gateway based on the $newpolicy6 variable
+ $newpolicy6 = New-AzureRmIpsecPolicy -IkeEncryption AES256 -IkeIntegrity SHA384 -DhGroup DHGroup24 `
+                                      -IpsecEncryption AES256 -IpsecIntegrity SHA256 -PfsGroup PFS24 `
                                        -SALifeTimeSeconds 28800 -SADataSizeKilobytes 4608000
 
-                          
-
-Set-AzureRmVirtualNetworkGatewayConnection -VirtualNetworkGatewayConnection $connection6 `
+                                       Set-AzureRmVirtualNetworkGatewayConnection -VirtualNetworkGatewayConnection $connection6 `
                                            -IpsecPolicies $newpolicy6 `
                                            -UsePolicyBasedTrafficSelectors $True
+#>
 
-
+# the below spits out the current config of the VPN gateway, representing whatever you did/didn't do to it
 $connection6  = Get-AzureRmVirtualNetworkGatewayConnection -Name $Connection16 -ResourceGroupName $RG1
 $connection6.IpsecPolicies
 
 
-Resize-AzureVNetGateway -VNetName $vngw1.Name -GatewaySKU "HighPerformance"
-
-$lngw = Get-AzureRmLocalNetworkGateway -Name "CAHCPSI" -ResourceGroupName $RG1
-
-
-# For resets
-
+<# To reset the VPN Gateway
 $gw = Get-AzureRmVirtualNetworkGateway -Name VNet1GW -ResourceGroup TestRG1
 Reset-AzureRmVirtualNetworkGateway -VirtualNetworkGateway $gw
+#>
